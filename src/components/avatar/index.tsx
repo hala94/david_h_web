f7ad9c2ec5de35446964ca740c8965baede6396a
David Halapir <david.halapir@decode.agency>
2020-08-05 12:52:19 +0200

    Layout progress

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,12 +1,12 @@
 import React, { CSSProperties } from 'react';
 import { Props } from 'interfaces/Props';
 
-export const RoundedAvatar = ({ className, imageSrc, ...props }: Props) => {
+export const Avatar = ({ className, imageSrc, ...props }: Props) => {
 	const imageStyle: CSSProperties = {
 		objectFit: 'cover',
-		height: '200px', // probably should be 100% of the containing element
-		width: '200px',
-		borderRadius: '50%',
+		height: '100%', // probably should be 100% of the containing element
+		width: '100%',
+		// borderRadius: '50%',
 	};
 
 	return <img src={imageSrc} alt="" style={imageStyle}></img>;
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import styles from './styles.module.css';
-import avatar from "assets/david_avatar.png";
+import avatar from 'assets/david_avatar.png';
 
-import { RoundedAvatar } from 'components/roundedAvatar';
+import { Avatar } from 'components/avatar';
 
 export function Home() {
 	return (
 		<div className={styles.container}>
-            <div></div>
+			<div className={styles.spacer}></div>
+			<div className={styles.navBar}>
+				<div className={styles.navBarItem}>Home</div>
+				<div className={styles.navBarItem}>Technologies</div>
+				<div className={styles.navBarItem}>Career</div>
+			</div>
+            <div className={styles.spacer2}></div>
+			<div className={styles.main_content_container}>
+				<div className={styles.content_item}>
+                    <div>Hello, i'm david</div>
+                    <div>MOBILE & FRONTEND DEVELOPER</div>
+            
+                </div>
+				<div className={styles.content_item}>
+                    <Avatar imageSrc={avatar}></Avatar>
+                </div>
+				<div className={styles.content_item}>Additonal Contact info dsj sdgjhgsd jhgdjs hgjshgds jhjhsgj dgshjgdshgdjshsdgj hsgdjdsgh jsdghs jshg</div>
+			</div>
 		</div>
 	);
 }
