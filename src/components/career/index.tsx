5d9c6884c3e9cb34896e644d97322b4b94436701
David Halapir <david.halapir@decode.agency>
2020-08-24 20:29:56 +0200

    added github button

--- src/components/career/index.tsx
+++ src/components/career/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './styles.module.css';
 import { Font, FontType } from 'helpers/fonts';
 import { Spacer, SpaceValue } from 'helpers/spacer';
+import { Button } from 'components/button';
+import { Asset } from 'helpers/asset';
 
 export function Carrer() {
 	return (
@@ -17,6 +19,12 @@ export function Carrer() {
 			<Font className={styles.career_item} fontType={FontType.careerItem}>
 				strong software design principles
 			</Font>
+			<Spacer v={SpaceValue.m}></Spacer>
+			<div className={styles.banner_container}>
+				<Button className={styles.github_button}>
+					<Asset className={styles.github_logo} name="github"></Asset>
+				</Button>
+			</div>
 		</div>
 	);
 }
