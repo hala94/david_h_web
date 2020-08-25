fb732fc88511db11867a9bb04e146e7d0c325344
David Halapir <david.halapir@decode.agency>
2020-08-25 20:41:27 +0200

    Updates

---
+++ static/js/3.de5d6bae.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio_david=this.webpackJsonpportfolio_david||[]).push([[3],{19:function(a,i,o){a.exports=o.p+"static/media/david_avatar.97abcf71.png"}}]);
+//# sourceMappingURL=3.de5d6bae.chunk.js.map
---
+++ static/js/4.8e79bf7c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio_david=this.webpackJsonpportfolio_david||[]).push([[4],{20:function(i,o,p){i.exports=p.p+"static/media/github.9ce68bca.png"}}]);
+//# sourceMappingURL=4.8e79bf7c.chunk.js.map
